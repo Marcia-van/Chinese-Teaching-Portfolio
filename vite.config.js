ed8fc81cbaa52ac2c56f11f4fd1973593edd7cf2
Marcia-van <104315400+Marcia-van@users.noreply.github.com>
2026-04-21 22:15:29 +0200

    fix: add base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 删掉了 base 路径，让它在本地根目录运行
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 加上这一行，必须和你的仓库名一致
+  base: '/Chinese-Teaching-Portfolio/',
 })
